620fcbb17f3e94e6bc6fddedb850e3f150c52c91
BulelaCool <102227388+BulelaCool@users.noreply.github.com>
2025-11-09 19:27:38 +0200

    Created ServiceRequestStatusController Updated Program.cs to register ServiceRequestStore

---
+++ Task 2 - Issue Report(Web)/Controllers/ServiceRequestStatusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task_2___Issue_Report_Web_.Controllers
+{
+    public class ServiceRequestStatusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
